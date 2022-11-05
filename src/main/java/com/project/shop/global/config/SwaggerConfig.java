ce295b6156f14c7aa1f04c0c72c3815a82c0710d
jong <dbswhd4932@gmail.com>
2022-11-05 10:18:16 +0900

    Swagger 추가

---
+++ src/main/java/com/project/shop/global/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.project.shop.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(true) // Swagger 에서 제공해주는 기본 응답 코드 (200, 401, 403, 404) 등의 노출 여부
+                .apiInfo(apiInfo()) // Swagger UI 로 노출할 정보
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.project.shop")) // api 스펙이 작성되어 있는 패키지 (controller)
+                .paths(PathSelectors.any()) // apis 에 위치하는 API 중 특정 path 를 선택
+                .build();
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("쇼핑몰 프로젝트")
+                .description("쇼핑몰 프로젝트 Swagger 문서")
+//                .version("")
+                .build();
+    }
+}
