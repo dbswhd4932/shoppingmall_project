7072819db6d17ae0985aa281007699214525a857
yoonjong <dbswhd4932@gmail.com>
2022-11-11 15:16:53 +0900

    refactor : 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,7 @@
 insert into ROLE (role) values ('USER');
+
+insert into MEMBER (login_id, password, name, zipcode, detail_address, email, phone, role_id)
+values ('테스트ID' , '1234', '홍길동', '123-123','상세주소','test@aaa.com','010-1234-1234-', 1);
+
+insert into GOODS (name, price, description)values ('테스트상품1' , '10000', '설명테스트1');
+insert into GOODS (name, price, description)values ('테스트상품2' , '20000', '설명테스트2');
