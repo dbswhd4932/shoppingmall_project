2e16a21f5187ce72bc5be1bd0bd2466f8ff010eb
yoonjong <dbswhd4932@gmail.com>
2022-11-12 00:14:53 +0900

    데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,14 @@
+-- 권한 추가
 insert into ROLE (role) values ('USER');
 
+-- 카테고리 추가
+insert into ITEM_CATEGORY (category) values ('의류');
+insert into ITEM_CATEGORY (category) values ('신발');
+
+-- 회원 추가 ( 권한 USER )
 insert into MEMBER (login_id, password, name, zipcode, detail_address, email, phone, role_id)
 values ('테스트ID' , '1234', '홍길동', '123-123','상세주소','test@aaa.com','010-1234-1234-', 1);
 
-insert into GOODS (name, price, description)values ('테스트상품1' , '10000', '설명테스트1');
-insert into GOODS (name, price, description)values ('테스트상품2' , '20000', '설명테스트2');
+-- 상품 추가
+insert into GOODS (name, price, description, item_category_id)values ('테스트상품1' , '10000', '설명테스트1', 1);
+insert into GOODS (name, price, description, item_category_id)values ('테스트상품2' , '20000', '설명테스트2', 2);
