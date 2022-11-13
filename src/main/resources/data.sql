e278ba8b0b159fa5180ec9e18b8827008d45f501
yoonjong <dbswhd4932@gmail.com>
2022-11-13 18:57:28 +0900

    feat : 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,5 +13,11 @@ insert into MEMBER (login_id, password, name, zipcode, detail_address, email, ph
 values ('테스트2' , '1234', '둘리', '456-678','상세주소','test2@aaa.com','010-4321-4321-', 1);
 
 -- 상품 추가
-insert into GOODS (name, price, description, item_category_id)values ('테스트상품1' , '10000', '설명테스트1', 1);
-insert into GOODS (name, price, description, item_category_id)values ('테스트상품2' , '20000', '설명테스트2', 2);
+insert into GOODS (name, price, description, item_category_id) values ('테스트상품1' , '10000', '설명테스트1', 1);
+insert into GOODS (name, price, description, item_category_id) values ('테스트상품2' , '20000', '설명테스트2', 2);
+
+-- 장바구니 추가
+insert into CART (goods_id, total_amount, total_price, member_id) values (1,1, 10000,1);
+insert into CART (goods_id, total_amount, total_price, member_id) values (2,1, 20000,1);
+insert into CART (goods_id, total_amount, total_price, member_id) values (1,1, 10000,2);
+insert into CART (goods_id, total_amount, total_price, member_id) values (2,1, 20000,2);
