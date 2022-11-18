f91cf87525364f825f8ea448b6bb12a435af39db
yoonjong <dbswhd4932@gmail.com>
2022-11-18 22:35:37 +0900

    feat : 리뷰 컨트롤러 테스트 추가

---
+++ src/test/java/com/project/shop/goods/controller/ReviewControllerTest.java
@@ -0,0 +1,57 @@
+package com.project.shop.goods.controller;
+
+import com.project.shop.factory.GoodsFactory;
+import com.project.shop.goods.controller.request.ReviewCreateRequest;
+import com.project.shop.goods.domain.Goods;
+import com.project.shop.goods.domain.Review;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ReviewController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+class ReviewControllerTest extends ControllerTest {
+
+    @Test
+    @DisplayName("리뷰생성")
+    void viewCreateTest() throws Exception {
+        //given
+        ReviewCreateRequest request = ReviewCreateRequest.builder()
+                .orderItemId(1L)
+                .memberId(1L)
+                .comment("댓글")
+                .build();
+
+        String json = objectMapper.writeValueAsString(request);
+
+        // when  then
+        mockMvc.perform(post("/api/reviews")
+                .contentType(APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isCreated());
+
+    }
+
+    @Test
+    @DisplayName("리뷰전체조회")
+    void reviewFindAllTest() throws Exception {
+        //given
+        Goods goods = GoodsFactory.createGoods();
+        Review review = Review.builder().memberId(1L).goods(goods).comment("리뷰").build();
+
+        String json = objectMapper.writeValueAsString(review);
+
+        //when then
+        mockMvc.perform(get("/api/reviews/all")
+                .contentType(APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk());
+
+    }
+}
