c3d1905fcbdc408fd87980f64c00d6f4a22d89e0
yoonjong <dbswhd4932@gmail.com>
2022-11-18 22:34:52 +0900

    feat : 테스트 관련 상속클래스 작성

---
+++ src/test/java/com/project/shop/goods/controller/ControllerTest.java
@@ -0,0 +1,26 @@
+package com.project.shop.goods.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.shop.goods.service.Impl.ReviewServiceImpl;
+import com.project.shop.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+public class ControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    ReviewServiceImpl reviewService;
+
+    @MockBean
+    MemberRepository memberRepository;
+
+
+}
