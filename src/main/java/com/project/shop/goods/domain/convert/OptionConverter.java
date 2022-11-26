0a0f722a36e54d856586a90b9e5af173aee3b836
yoonjong <dbswhd4932@gmail.com>
2022-11-26 18:42:47 +0900

    수정

--- src/main/java/com/project/shop/goods/domain/convert/OptionConverter.java
+++
@@ -1,4 +0,0 @@
-package com.project.shop.goods.domain.convert;
-
-public class OptionConverter extends GenericJsonConverter{
-}
--- src/main/java/com/project/shop/goods/repository/OptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.shop.goods.repository;
-
-import com.project.shop.goods.domain.Option;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OptionRepository extends JpaRepository<Option, Long> {
-}
