672b6785ea6fba898ee24ed2c64dfe928f641f6c
yoonjong <dbswhd4932@gmail.com>
2022-11-26 18:51:37 +0900

    Revert "Revert "수정""
    This reverts commit 19fb401ded25eb8d9b4d4a0eb7a8a028de133c91.

--- src/main/java/com/project/shop/order/domain/OrderStatus.java
+++
@@ -1,7 +0,0 @@
-package com.project.shop.order.domain;
-
-public enum OrderStatus {
-
-    COMPLETE, // 결제완료
-    CANCEL, // 결제취소
-}
--- src/main/java/com/project/shop/order/repository/PayRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.shop.order.repository;
-
-import com.project.shop.order.domain.Pay;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PayRepository extends JpaRepository<Pay, Long> {
-
-}
