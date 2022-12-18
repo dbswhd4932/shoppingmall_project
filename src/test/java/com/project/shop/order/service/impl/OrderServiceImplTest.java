175f3ba0a3f627244baf04b0c15ecf2009a93ff6
yoonjong <dbswhd4932@gmail.com>
2022-12-18 21:22:54 +0900

    delete

--- src/test/java/com/project/shop/order/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/project/shop/order/service/impl/OrderServiceImplTest.java
@@ -1,34 +1,14 @@
 package com.project.shop.order.service.impl;
 
-import com.project.shop.factory.CardFactory;
-import com.project.shop.factory.CartFactory;
-import com.project.shop.factory.GoodsFactory;
-import com.project.shop.factory.MemberFactory;
-import com.project.shop.goods.domain.Goods;
 import com.project.shop.goods.repository.GoodsRepository;
-import com.project.shop.member.domain.Card;
-import com.project.shop.member.domain.Cart;
-import com.project.shop.member.domain.Member;
-import com.project.shop.member.repository.CardRepository;
 import com.project.shop.member.repository.CartRepository;
-import com.project.shop.order.controller.request.OrderCreateRequest;
-import com.project.shop.order.domain.Order;
-import com.project.shop.order.domain.OrderStatus;
 import com.project.shop.order.repository.OrderRepository;
 import com.project.shop.order.repository.PayRepository;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
