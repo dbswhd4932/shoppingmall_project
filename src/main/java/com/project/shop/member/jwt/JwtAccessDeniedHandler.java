9133b21e17951679322c487ce82676a9f67abe97
yoonjong <dbswhd4932@gmail.com>
2022-12-18 18:25:45 +0900

    feat : log 추가

--- src/main/java/com/project/shop/member/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/project/shop/member/jwt/JwtAccessDeniedHandler.java
@@ -1,5 +1,6 @@
 package com.project.shop.member.jwt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -10,11 +11,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException, IOException {
         // 필요한 권한이 없이 접근하려 할때 403
+        log.info("권한을 확인하세요.");
         response.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
 }
