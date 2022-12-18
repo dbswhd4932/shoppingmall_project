be0cfe941f4e373905e5e51f32a9ae70b35e3e60
yoonjong <dbswhd4932@gmail.com>
2022-12-18 21:10:32 +0900

    feat : log 추가

--- src/main/java/com/project/shop/member/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/project/shop/member/jwt/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 package com.project.shop.member.jwt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -9,11 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, IOException {
         // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
+        log.info("아이디 또는 비밀번호를 확인하세요.");
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
