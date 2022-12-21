152ac7f4d468e1a86db1d59dabe2f90ec461b9c6
yoonjong <dbswhd4932@gmail.com>
2022-12-21 15:42:00 +0900

    refactor : repository 미사용 메서드 정리

--- src/main/java/com/project/shop/member/repository/CartRepository.java
+++ src/main/java/com/project/shop/member/repository/CartRepository.java
@@ -3,7 +3,6 @@ package com.project.shop.member.repository;
 import com.project.shop.member.domain.Cart;
 import com.project.shop.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,8 +10,6 @@ import java.util.Optional;
 public interface CartRepository  extends JpaRepository<Cart, Long> {
 
     Optional<List<Cart>> findByMemberId(Long memberId);
-    Optional<Cart> findByGoodsId(Long goodsId);
-    Optional<Cart> findByIdAndGoodsIdAndMemberId(Long cartId, Long goodsId , Long memberId);
 
     Optional<Cart> findByIdAndMember(Long cartId, Member member);
 
