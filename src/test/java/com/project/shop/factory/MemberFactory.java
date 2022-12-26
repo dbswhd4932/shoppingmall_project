dfbb3e15567d6b9124a000e1f4b2eb00756dcd31
yoonjong <dbswhd4932@gmail.com>
2022-12-26 16:09:51 +0900

    feat : PasswordEncoder 추가

--- src/test/java/com/project/shop/factory/MemberFactory.java
+++ src/test/java/com/project/shop/factory/MemberFactory.java
@@ -1,32 +1,46 @@
 package com.project.shop.factory;
 
-import com.project.shop.member.domain.Member;
 import com.project.shop.member.controller.request.MemberSignupRequest;
+import com.project.shop.member.domain.LoginType;
+import com.project.shop.member.domain.Member;
+import com.project.shop.member.domain.RoleType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
 
 public class MemberFactory {
 
-    public static Member createMember() {
+    PasswordEncoder passwordEncoder;
+
+    public MemberFactory(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Member createMember() {
         Member member = Member.builder()
                 .loginId("loginId")
-                .password("1234")
+                .password(passwordEncoder.encode("1234"))
                 .name("name")
                 .zipcode("123-123")
                 .detailAddress("seoul")
                 .email("user@test.com")
                 .phone("010-1111-1111")
+                .loginType(LoginType.NO_SOCIAL)
                 .build();
 
         return member;
     }
 
-    public static MemberSignupRequest createSignupRequestDto() {
+    public MemberSignupRequest createSignupRequestDto() {
         MemberSignupRequest memberSignupRequest = MemberSignupRequest.builder()
-                .password("1234")
+                .loginId("loginId")
+                .password(passwordEncoder.encode("1234"))
                 .name("name")
                 .zipcode("123-123")
                 .detailAddress("seoul")
                 .email("user@test.com")
                 .phone("010-1111-1111")
+                .roles(List.of(RoleType.ROLE_USER))
                 .build();
 
         return memberSignupRequest;
