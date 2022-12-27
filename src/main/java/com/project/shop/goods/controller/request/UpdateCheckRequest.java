3483af635c465bbaf5cd7bccf90572239b96780f
yoonjong <dbswhd4932@gmail.com>
2022-12-27 16:02:00 +0900

    feat : builder 추가

--- src/main/java/com/project/shop/goods/controller/request/UpdateCheckRequest.java
+++ src/main/java/com/project/shop/goods/controller/request/UpdateCheckRequest.java
@@ -1,11 +1,14 @@
 package com.project.shop.goods.controller.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UpdateCheckRequest {
 
     private Long goodsId;
