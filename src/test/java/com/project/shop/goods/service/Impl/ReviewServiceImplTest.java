8b1b9fa008eff539e35e3056588a38cd08a81024
yoonjong <dbswhd4932@gmail.com>
2022-12-27 14:12:35 +0900

    feat : 단위테스트 추가

--- src/test/java/com/project/shop/goods/service/Impl/ReviewServiceImplTest.java
+++ src/test/java/com/project/shop/goods/service/Impl/ReviewServiceImplTest.java
@@ -2,8 +2,6 @@ package com.project.shop.goods.service.Impl;
 
 import com.project.shop.factory.GoodsFactory;
 import com.project.shop.factory.MemberFactory;
-import com.project.shop.global.error.ErrorCode;
-import com.project.shop.global.error.exception.BusinessException;
 import com.project.shop.goods.controller.request.ReviewCreateRequest;
 import com.project.shop.goods.controller.request.ReviewEditRequest;
 import com.project.shop.goods.controller.response.ReviewResponse;
@@ -16,8 +14,9 @@ import com.project.shop.member.repository.MemberRepository;
 import com.project.shop.order.domain.Order;
 import com.project.shop.order.domain.OrderItem;
 import com.project.shop.order.repository.OrderItemRepository;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -30,16 +29,13 @@ import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.test.context.support.WithMockUser;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
@@ -129,7 +125,6 @@ class ReviewServiceImplTest {
         Review review = new Review(1L, goods, "comment");
         given(reviewRepository.findById(1L)).willReturn(Optional.of(review));
         given(memberRepository.findByLoginId(member.getLoginId())).willReturn(Optional.ofNullable(member));
-//        assertThatCode(() -> review.getMemberId().equals(member.getId())).doesNotThrowAnyException();
 
         ReviewEditRequest editComment = new ReviewEditRequest("editComment");
         //when
--- src/test/java/com/project/shop/order/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/project/shop/order/service/impl/OrderServiceImplTest.java
@@ -1,14 +1,47 @@
 package com.project.shop.order.service.impl;
 
+import com.project.shop.factory.CartFactory;
+import com.project.shop.factory.GoodsFactory;
+import com.project.shop.factory.MemberFactory;
+import com.project.shop.factory.OrderFactory;
+import com.project.shop.goods.domain.Goods;
 import com.project.shop.goods.repository.GoodsRepository;
+import com.project.shop.member.domain.Cart;
+import com.project.shop.member.domain.Member;
 import com.project.shop.member.repository.CartRepository;
+import com.project.shop.member.repository.MemberRepository;
+import com.project.shop.order.controller.request.OrderCreateRequest;
+import com.project.shop.order.controller.request.PayCancelRequest;
+import com.project.shop.order.controller.response.OrderResponse;
+import com.project.shop.order.domain.Order;
+import com.project.shop.order.domain.Pay;
+import com.project.shop.order.domain.PayCancel;
 import com.project.shop.order.repository.OrderRepository;
+import com.project.shop.order.repository.PayCancelRepository;
 import com.project.shop.order.repository.PayRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,11 +59,93 @@ class OrderServiceImplTest {
     @Mock
     PayRepository payRepository;
 
-//    @Mock
-//    CardRepository cardRepository;
 
-//    @Mock
-//    GoodsRepository goodsRepository;
+    @Mock
+    PayCancelRepository payCancelRepository;
 
+    @Mock
+    GoodsRepository goodsRepository;
+
+    @Mock
+    MemberRepository memberRepository;
+
+    @Mock
+    PasswordEncoder passwordEncoder;
+
+    @BeforeEach()
+    void beforeEach() {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        TestingAuthenticationToken mockAuthentication = new TestingAuthenticationToken("loginId", "1234");
+        context.setAuthentication(mockAuthentication);
+        SecurityContextHolder.setContext(context);
+    }
+
+    @Test
+    @DisplayName("주문 생성")
+    void cartOrder() {
+        //given
+        MemberFactory memberFactory = new MemberFactory(passwordEncoder);
+        Member member = memberFactory.createMember();
+        Goods goods = GoodsFactory.createGoods();
+        Cart cart = CartFactory.cartCreate(member, goods);
+        OrderCreateRequest orderCreateRequest = OrderFactory.orderCreateRequest(goods);
+        given(memberRepository.findByLoginId(member.getLoginId())).willReturn(Optional.of(member));
+        given(goodsRepository.findById(orderCreateRequest.getOrderItemCreates().get(0).getGoodsId()))
+                .willReturn(Optional.of(goods));
+        given(cartRepository.findByGoodsIdAndMember(goods.getId(), member)).willReturn(Optional.ofNullable(cart));
+
+        //when
+        orderService.cartOrder(orderCreateRequest);
+
+        //then
+        verify(cartRepository).deleteById(any());
+        verify(orderRepository).save(any());
+        verify(payRepository).save(any());
+    }
+
+    @Test
+    @DisplayName("주문 조회")
+    void orderFindMember() {
+        //given
+        MemberFactory memberFactory = new MemberFactory(passwordEncoder);
+        Member member = memberFactory.createMember();
+        Order order = OrderFactory.order(member);
+        Pageable pageable = PageRequest.of(0, 10, Sort.Direction.DESC, "id");
+        List<Order> list = new ArrayList<>();
+        list.add(order);
+
+        PageImpl<Order> orders = new PageImpl<>(list);
+
+        given(memberRepository.findByLoginId(member.getLoginId())).willReturn(Optional.of(member));
+        given(orderRepository.findAll(pageable)).willReturn(orders);
+
+        //when
+        List<OrderResponse> orderResponses = orderService.orderFindMember(pageable);
+
+        //then
+        assertThat(orderResponses.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("주문 취소")
+    void payCancel() {
+        //given
+        MemberFactory memberFactory = new MemberFactory(passwordEncoder);
+        Member member = memberFactory.createMember();
+        Order order = OrderFactory.order(member);
+        Pay pay = new Pay("cardCompany", "1111-1111-1111-1111", order, 10000);
+        PayCancel payCancel = new PayCancel(order, "1111", "reason", 10000, "cardCompany", "1111-1111-1111-1111");
+        PayCancelRequest payCancelRequest = new PayCancelRequest("1111", "reason");
+
+        given(memberRepository.findByLoginId(member.getLoginId())).willReturn(Optional.of(member));
+        given(payRepository.findById(pay.getId())).willReturn(Optional.of(pay));
+
+        //when
+        orderService.payCancel(pay.getId(), payCancelRequest);
+
+        //then
+        verify(payCancelRepository).save(any());
+
+    }
 
 }
