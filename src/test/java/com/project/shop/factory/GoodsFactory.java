c0c48e76f0fbe6dee2cf42a1f59ae3a6332d259e
yoonjong <dbswhd4932@gmail.com>
2022-12-28 23:15:38 +0900

    feat : 테스트 편리성을 위한 goodsCreateRequest 구현

--- src/test/java/com/project/shop/factory/GoodsFactory.java
+++ src/test/java/com/project/shop/factory/GoodsFactory.java
@@ -1,5 +1,6 @@
 package com.project.shop.factory;
 
+import com.project.shop.goods.controller.request.GoodsCreateRequest;
 import com.project.shop.goods.domain.Category;
 import com.project.shop.goods.domain.Goods;
 
@@ -14,6 +15,16 @@ public class GoodsFactory {
                 .category(new Category("테스트카테고리"))
                 .price(1000)
                 .description("테스트 상품 설명")
+                .build();
+    }
+
+    public static GoodsCreateRequest createRequest() {
+        return GoodsCreateRequest.builder()
+                .goodsName("테스트상품")
+                .optionCreateRequest(null)
+                .category(new Category("테스트카테고리"))
+                .goodsDescription("테스트 상품 설명")
+                .price(10000)
                 .build();
     }
 
