bea3be349519d3ff3a89801379d294ac7701c3c1
yoonjong <dbswhd4932@gmail.com>
2022-12-28 14:32:13 +0900

    feat : 테스트를 위한 SecurityConfig 추가

---
+++ src/test/java/com/project/shop/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.project.shop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+
+        return http.build();
+    }
+}
