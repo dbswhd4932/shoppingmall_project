b193124f1342caf85dc12c59e47b89a9c82bb2f8
yoonjong <dbswhd4932@gmail.com>
2022-12-30 01:10:26 +0900

    refactor : 미사용 메서드 삭제

--- src/main/java/com/project/shop/member/repository/MemberRepository.java
+++ src/main/java/com/project/shop/member/repository/MemberRepository.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByLoginId(String loginId);
-    Optional<Member> findByLoginIdAndPassword(String loginId, String password);
     Optional<Member> findByEmailAndLoginId(String email, String loginId);
 
 }
--- src/main/java/com/project/shop/member/repository/RoleRepository.java
+++ src/main/java/com/project/shop/member/repository/RoleRepository.java
@@ -8,5 +8,4 @@ import java.util.List;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    List<Role> findByMember(Member member);
 }
