45c51b2a309b73d0faefccf10a26231581f0b7fa
yoonjong <dbswhd4932@gmail.com>
2022-12-30 23:53:48 +0900

    refactor : CartController 통합테스트 재수정

--- src/test/java/com/project/shop/member/controller/CartControllerTest.java
+++ src/test/java/com/project/shop/member/controller/CartControllerTest.java
@@ -1,49 +1,84 @@
 package com.project.shop.member.controller;
 
 import com.project.shop.config.WebSecurityConfig;
+import com.project.shop.factory.GoodsFactory;
+import com.project.shop.factory.MemberFactory;
+import com.project.shop.goods.controller.request.OptionCreateRequest;
+import com.project.shop.goods.domain.Goods;
+import com.project.shop.goods.domain.Option;
+import com.project.shop.goods.domain.OptionCreate;
 import com.project.shop.member.controller.request.CartCreateRequest;
-import com.project.shop.member.controller.request.CartEditRequest;
-import com.project.shop.member.controller.response.CartResponse;
+import com.project.shop.member.domain.Member;
 import com.project.shop.member.repository.CartRepository;
-import com.project.shop.member.service.Impl.CartServiceImpl;
+import com.project.shop.member.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.refEq;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(CartController.class)
-@MockBean(JpaMetamodelMappingContext.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 @ImportAutoConfiguration(WebSecurityConfig.class)
-@DisplayName("장바구니 컨트롤러 테스트")
+@WebAppConfiguration
+@DisplayName("컨트롤러 장바구니 통합테스트")
 class CartControllerTest extends ControllerSetting {
 
-    @MockBean
-    CartServiceImpl cartService;
-
-    @MockBean
+    @Autowired
     CartRepository cartRepository;
 
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    WebApplicationContext context;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void beforeEach() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .alwaysDo(print())
+                .apply(springSecurity())
+                .build();
+    }
+
     @Test
-    @DisplayName("장바구니 담기")
-    @WithMockUser(roles = "USER")
-    void cartAddGoodsTest() throws Exception {
+    @WithMockUser(username = "loginId" , roles = "USER")
+    @DisplayName("장바구니 상품 추가")
+    void cartAddGoods() throws Exception {
         //given
-        CartCreateRequest cartCreateRequest
-                = CartCreateRequest.builder().goodsId(1L).amount(5).build();
+
+        MemberFactory memberFactory = new MemberFactory(passwordEncoder);
+        Member member = memberFactory.createMember();
+        memberRepository.save(member);
+        OptionCreate optionCreate = OptionCreate.builder().key("key").value("value").build();
+        OptionCreateRequest optionCreateRequest = OptionCreateRequest.builder()
+                .totalPrice(1000).optionValue(List.of(optionCreate)).optionDescription("설명").build();
+        Goods goods = GoodsFactory.createGoods();
+        Option option = Option.toOption(optionCreateRequest, goods);
+        CartCreateRequest cartCreateRequest = CartCreateRequest.builder()
+                .optionNumber(option.getId()).amount(10).goodsId(goods.getId()).build();
 
         //when
         mockMvc.perform(post("/api/carts")
@@ -52,55 +87,8 @@ class CartControllerTest extends ControllerSetting {
                 .andExpect(status().isCreated());
 
         //then
-        verify(cartService).cartAddGoods(refEq(cartCreateRequest));
-    }
+        Assertions.assertThat(cartRepository.findAll().size()).isEqualTo(1);
 
-    @Test
-    @DisplayName("장바구니 조회")
-    @WithMockUser(roles = "USER")
-    void cartFindTest() throws Exception {
-        //given
-        CartResponse cartResponse
-                = CartResponse.builder().memberId(1L).goodsId(1L).totalPrice(1000).totalAmount(10).build();
-        given(cartService.cartFindMember()).willReturn(List.of(cartResponse));
-
-        //when then
-        mockMvc.perform(get("/api/carts")
-                        .contentType(APPLICATION_JSON))
-                .andExpect(jsonPath("$.[0].totalAmount").value(10))
-                .andExpect(jsonPath("$.[0].totalPrice").value(1000))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    @DisplayName("장바구니 수정")
-    @WithMockUser(roles = "USER")
-    void cartEdit() throws Exception {
-        //given
-        CartEditRequest cartEditRequest
-                = CartEditRequest.builder().amount(1000).optionNumber(1L).build();
-
-        //when
-        mockMvc.perform(put("/api/carts/{cartId}", 1L)
-                .contentType(APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(cartEditRequest)))
-                .andExpect(status().isOk());
-
-        //then
-        verify(cartService).editCartItem(anyLong(), refEq(cartEditRequest));
-    }
-
-    @Test
-    @DisplayName("장바구니 삭제")
-    @WithMockUser(roles = "USER")
-    void cartDeleteGoodsTest() throws Exception {
-        //given
-        //when
-        mockMvc.perform(delete("/api/carts/{cartId}", 1L))
-                .andExpect(status().isNoContent());
-
-        //then
-        verify(cartService).cartDeleteGoods(1L);
 
     }
 
