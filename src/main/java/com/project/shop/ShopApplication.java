957c8f83f564a0dfb7f976ad9598ba8d15c3ec24
yoonjong <dbswhd4932@gmail.com>
2023-01-03 00:55:28 +0900

    refactor : Ehcache2 -> Ehcache3 로 변경

--- src/main/java/com/project/shop/ShopApplication.java
+++ src/main/java/com/project/shop/ShopApplication.java
@@ -1,13 +1,13 @@
 package com.project.shop;
 
-import com.project.shop.global.config.S3Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class ShopApplication {
 
     public static void main(String[] args) {
