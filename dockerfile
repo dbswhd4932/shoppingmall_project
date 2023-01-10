6918c98c351dcd1fa0c82ac255f6dbe0c60f9417
yoonjong <dbswhd4932@gmail.com>
2023-01-10 12:01:49 +0900

    refactor : 통합테스트 제외 test

--- dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:17
-ARG JAR_FILE=build/libs/*.jar
-COPY ${JAR_FILE} shop.jar
-ENTRYPOINT ["java","-jar","/shop.jar"]
---
+++ src/main/java/com/project/shop/CicdHealthCkeck.java
@@ -0,0 +1,13 @@
+package com.project.shop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CicdHealthCkeck {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "CICD test";
+    }
+}
