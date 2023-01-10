627c7a53fe75a89a8cf7a1e9e6116924ed60ec7c
yoonjong <dbswhd4932@gmail.com>
2023-01-10 22:59:18 +0900

    refactor : CI/CD 수정

--- src/main/java/com/project/shop/global/aspect/MemberLoginAop.java
+++ src/main/java/com/project/shop/global/aspect/MemberLoginAop.java
@@ -1,30 +1,30 @@
-//package com.project.shop.global.aspect;
-//
-//import com.project.shop.member.controller.request.LoginRequest;
-//import com.project.shop.member.domain.MemberLoginHistory;
-//import com.project.shop.member.repository.MemberLoginHistoryRepository;
-//import lombok.AllArgsConstructor;
-//import org.aspectj.lang.annotation.AfterReturning;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Pointcut;
-//import org.springframework.stereotype.Component;
-//
-//import java.time.LocalDateTime;
-//
-//@Aspect
-//@Component // Bean 으로 등록
-//@AllArgsConstructor
-//public class MemberLoginAop {
-//
-//    private MemberLoginHistoryRepository memberLoginHistoryRepository;
-//
-//    @Pointcut("execution(* com.project.shop.member.service.MemberService.login(*)) && args(loginRequest)")
-//    public void loginMember(LoginRequest loginRequest){}
-//
-//    @AfterReturning(value = "loginMember(loginRequest)", argNames = "loginRequest")
-//    public void saveMemberHistory(LoginRequest loginRequest) {
-//        MemberLoginHistory memberLoginHistory = MemberLoginHistory.builder().loginId(loginRequest.getLoginId()).time(LocalDateTime.now()).build();
-//        memberLoginHistoryRepository.save(memberLoginHistory);
-//    }
-//
-//}
+package com.project.shop.global.aspect;
+
+import com.project.shop.member.controller.request.LoginRequest;
+import com.project.shop.member.domain.MemberLoginHistory;
+import com.project.shop.member.repository.MemberLoginHistoryRepository;
+import lombok.AllArgsConstructor;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Aspect
+@Component // Bean 으로 등록
+@AllArgsConstructor
+public class MemberLoginAop {
+
+    private MemberLoginHistoryRepository memberLoginHistoryRepository;
+
+    @Pointcut("execution(* com.project.shop.member.service.MemberService.login(*)) && args(loginRequest)")
+    public void loginMember(LoginRequest loginRequest){}
+
+    @AfterReturning(value = "loginMember(loginRequest)", argNames = "loginRequest")
+    public void saveMemberHistory(LoginRequest loginRequest) {
+        MemberLoginHistory memberLoginHistory = MemberLoginHistory.builder().loginId(loginRequest.getLoginId()).time(LocalDateTime.now()).build();
+        memberLoginHistoryRepository.save(memberLoginHistory);
+    }
+
+}
