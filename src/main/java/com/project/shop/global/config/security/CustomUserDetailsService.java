e4d732b5406b880ca11e7919168c60044409e510
yoonjong <dbswhd4932@gmail.com>
2023-01-12 10:58:59 +0900

    feat : 미사용 클래스 선 주석처리

--- src/main/java/com/project/shop/global/config/security/CustomUserDetailsService.java
+++ src/main/java/com/project/shop/global/config/security/CustomUserDetailsService.java
@@ -1,44 +1,44 @@
-package com.project.shop.global.config.security;
-
-import com.project.shop.member.domain.Member;
-import com.project.shop.member.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Collections;
-
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
-
-    private final MemberRepository memberRepository;
-
-    // 시큐리티 세션 -> Authentication -> UserDetails
-    @Override
-    @Transactional
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return memberRepository.findByLoginId(username)
-                .map(member -> createUserDetails(member))
-                .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
-    }
-
-    // DB 에 User 값이 존재한다면 UserDetails 객체로 만들어서 리턴
-    private UserDetails createUserDetails(Member member) {
-        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getRoles().toString());
-        // 일반로그인, 카카오 로그인 같이 사용 가능
-        return new User(
-                String.valueOf(member.getLoginId()),
-                member.getPassword(),
-                Collections.singleton(grantedAuthority)
-        );
-    }
-}
+//package com.project.shop.global.config.security;
+//
+//import com.project.shop.member.domain.Member;
+//import com.project.shop.member.repository.MemberRepository;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.Collections;
+//
+//@Slf4j
+//@Service
+//@RequiredArgsConstructor
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//    private final MemberRepository memberRepository;
+//
+//    // 시큐리티 세션 -> Authentication -> UserDetails
+//    @Override
+//    @Transactional
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        return memberRepository.findByLoginId(username)
+//                .map(member -> createUserDetails(member))
+//                .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
+//    }
+//
+//    // DB 에 User 값이 존재한다면 UserDetails 객체로 만들어서 리턴
+//    private UserDetails createUserDetails(Member member) {
+//        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getRoles().toString());
+//        // 일반로그인, 카카오 로그인 같이 사용 가능
+//        return new User(
+//                String.valueOf(member.getLoginId()),
+//                member.getPassword(),
+//                Collections.singleton(grantedAuthority)
+//        );
+//    }
+//}
