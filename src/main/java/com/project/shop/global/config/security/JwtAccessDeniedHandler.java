0e35fb3d114eefec641efc53cf4311aaf259b52e
yoonjong <dbswhd4932@gmail.com>
2023-01-14 15:46:41 +0900

    refactor : 예외 코드 수정

--- src/main/java/com/project/shop/global/config/security/JwtAccessDeniedHandler.java
+++ src/main/java/com/project/shop/global/config/security/JwtAccessDeniedHandler.java
@@ -19,7 +19,6 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         // 필요한 권한이 없이 접근하려 할때 403
         log.info("권한을 확인하세요.");
-        throw new BusinessException(ErrorCode.NOT_FOUND_AUTHORITY);
-//        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
 }
