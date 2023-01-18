2e0595d32131c92597fd036285f0126c768aac0f
yoonjong <dbswhd4932@gmail.com>
2023-01-18 21:29:26 +0900

    PR 테스트

--- src/main/java/com/project/shop/CicdHealthCkeck.java
+++ src/main/java/com/project/shop/CicdHealthCkeck.java
@@ -7,21 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CicdHealthCkeck {
 
-
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.access-key}")
-    private String secretKey;
-
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
-
-    @Value("${cloud.aws.region.static}")
-    private String region;
-
     @GetMapping("/hello")
     public String hello() {
-        return "CI CD test update2222";
+        return "PR test";
     }
 }
