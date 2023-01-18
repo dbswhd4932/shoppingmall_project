ba6c156d04092aa826c9eb4f4ef954cb16011fd8
yoonjong <dbswhd4932@gmail.com>
2023-01-18 13:34:36 +0900

    feat : aws secret 매니저 추가

--- src/main/java/com/project/shop/CicdHealthCkeck.java
+++ src/main/java/com/project/shop/CicdHealthCkeck.java
@@ -1,13 +1,27 @@
 package com.project.shop;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CicdHealthCkeck {
 
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
     @GetMapping("/hello")
     public String hello() {
-        return "CICD test!";
+        return bucket + accessKey + secretKey + region;
     }
 }
