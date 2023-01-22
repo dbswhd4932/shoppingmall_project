5151a28011208a232231f5eee551cd4c610e9555
yoonjong <dbswhd4932@gmail.com>
2023-01-22 12:56:09 +0900

    refactor : Test DB 분리

--- src/test/java/com/project/shop/goods/controller/GoodsControllerTest.java
+++ src/test/java/com/project/shop/goods/controller/GoodsControllerTest.java
@@ -22,6 +22,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
--- src/test/java/com/project/shop/member/controller/ControllerSetting.java
+++ src/test/java/com/project/shop/member/controller/ControllerSetting.java
@@ -2,8 +2,13 @@ package com.project.shop.member.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
+@TestPropertySource(properties = "spring.config.location=classpath:application-test.yml")
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class ControllerSetting {
 
     @Autowired
