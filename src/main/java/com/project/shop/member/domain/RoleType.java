636c80bb4733ed17395b2e9fe4b5f674ba913de5
r00416 <dbswhd4932@gmail.com>
2025-11-07 17:44:31 +0900

    회원가입 / 로그인 에러 수정

--- src/main/java/com/project/shop/member/domain/RoleType.java
+++ src/main/java/com/project/shop/member/domain/RoleType.java
@@ -1,5 +1,6 @@
 package com.project.shop.member.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum RoleType {
     ROLE_USER("ROLE_USER"),
@@ -14,6 +15,29 @@ public enum RoleType {
 
     public String value() {
         return role;
+    }
+
+    @JsonCreator
+    public static RoleType from(String value) {
+        // "SELLER" 또는 "ROLE_SELLER" 모두 처리
+        if (value == null) {
+            return null;
+        }
+
+        String upperValue = value.toUpperCase();
+
+        // "ROLE_" prefix가 없으면 추가
+        if (!upperValue.startsWith("ROLE_")) {
+            upperValue = "ROLE_" + upperValue;
+        }
+
+        for (RoleType roleType : RoleType.values()) {
+            if (roleType.role.equals(upperValue)) {
+                return roleType;
+            }
+        }
+
+        throw new IllegalArgumentException("Unknown role type: " + value);
     }
 
 }
